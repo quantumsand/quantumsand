96705b21b7d1a90375d3e09513738764666205bf
Andrew Potter <hello@drewpotter.com>
2025-04-11 16:13:05 +0100

    Add logs

--- quantumsand-live/lib/quantumsand_live/format_stick.rb
+++ quantumsand-live/lib/quantumsand_live/format_stick.rb
@@ -2,7 +2,9 @@ class QuantumsandLive::FormatStick
   def self.hi(root_password)
     puts "Hello world! QuantumsandLive!! FormatStick"
     puts "Testing root password by listing sudo cat '/etc/sudoers'"
-    system("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'")
+    sudoers_contents = system("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'", 
+                              out: ['/tmp/quantumsand_out.log', 'a'], err: ['/tmp/quantumsand_err.log', 'a'])
+    puts "Contents from sudoers... #{sudoers_contents}"
     puts "Testing non root echo *"
     system("sudo echo *")
   end
