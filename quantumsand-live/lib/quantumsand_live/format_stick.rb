602cd1a892a8ac7e56fb350edb0f655b3d222b30
Andrew Potter <hello@drewpotter.com>
2025-04-11 16:34:32 +0100

    Improve switch to Open3.capture3 away from system

--- quantumsand-live/lib/quantumsand_live/format_stick.rb
+++ quantumsand-live/lib/quantumsand_live/format_stick.rb
@@ -1,9 +1,16 @@
+require 'open3'
+
 class QuantumsandLive::FormatStick
   def self.hi(root_password)
     puts "Hello world! QuantumsandLive!! FormatStick"
     puts "Testing root password by listing sudo cat '/etc/sudoers'"
-    sudoers_contents = system("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'", 
-                              out: ['/tmp/quantumsand_out.log', 'a'], err: ['/tmp/quantumsand_err.log', 'a'])
+
+    stdout, stderr, status = Open3.capture3("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'")
+    puts "Output: #{stdout}"
+    puts "Error: #{stderr}" if stderr
+    puts "Exit Status: #{status.exitstatus}"
+
+    sudoers_contents = stdout
     puts "Contents from sudoers... #{sudoers_contents}"
     puts "Testing non root echo *"
     system("sudo echo *")
