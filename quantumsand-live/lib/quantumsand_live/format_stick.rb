147ab12bec480d5d86eb9990a7aef37340203f36
Andrew Potter <hello@drewpotter.com>
2025-04-11 15:42:43 +0100

    Typo

--- quantumsand-live/lib/quantumsand_live/format_stick.rb
+++ quantumsand-live/lib/quantumsand_live/format_stick.rb
@@ -1,7 +1,7 @@
 class QuantumsandLive::FormatStick
   def self.hi(root_password)
     puts "Hello world! QuantumsandLive!! FormatStick"
-    puts "Testing root password my listing sudo cat '/etc/sudoers'"
+    puts "Testing root password by listing sudo cat '/etc/sudoers'"
     system("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'")
     puts "Testing non root echo *"
     system("sudo echo *")
