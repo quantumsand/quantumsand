e91ab61e2f6ab6b3286f9c12468d15571ece0c5c
Andrew Potter <hello@drewpotter.com>
2025-04-11 17:12:11 +0100

    Improve

--- quantumsand-live/lib/quantumsand_live/format_stick.rb
+++ quantumsand-live/lib/quantumsand_live/format_stick.rb
@@ -5,7 +5,7 @@ class QuantumsandLive::FormatStick
     puts "Hello world! QuantumsandLive!! FormatStick"
     puts "Testing root password by listing sudo cat '/etc/sudoers'"
 
-    stdout, stderr, status = Open3.capture3("sudo -S <<< '#{root_password}' sudo cat '/etc/sudoers'")
+    stdout, stderr, status = Open3.capture3("sudo -S <<< '#{root_password}' cat '/etc/sudoers'")
     puts "Output: #{stdout}"
     puts "Error: #{stderr}" if stderr
     puts "Exit Status: #{status.exitstatus}"
@@ -13,6 +13,6 @@ class QuantumsandLive::FormatStick
     sudoers_contents = stdout
     puts "Contents from sudoers... #{sudoers_contents}"
     puts "Testing non root echo *"
-    system("sudo echo *")
+    system("echo *")
   end
 end
