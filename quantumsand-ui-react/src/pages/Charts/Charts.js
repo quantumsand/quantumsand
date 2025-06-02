470d22843c22a4d086f2f7084093991b97a0dd6a
Andrew Potter <hello@drewpotter.com>
2025-06-02 13:45:33 +0100

    Switch Chart to ResponsivePie

--- quantumsand-ui-react/src/pages/Charts/Charts.js
+++ quantumsand-ui-react/src/pages/Charts/Charts.js
@@ -1,41 +1,131 @@
-import { ResponsiveBullet } from "@nivo/bullet";
+import { useState, useEffect } from 'react';
+import { ResponsivePie } from "@nivo/pie";
 import "./Charts.css";
 
-const data = [
-  {
-    "id": "",
-    "ranges": [
-      0.3336816061121536,
-      0.2184509493483253,
-      1.5839206391675538,
-      0,
-      2
-    ],
-    "measures": [
-      0.015011141606366117,
-      0.03454565234304585
-    ],
-    "markers": [
-      1.5637346292993346
-    ]
-  }
-]
+// const data = [
+//   {
+//     "id": "",
+//     "ranges": [
+//       0.3336816061121536,
+//       0.2184509493483253,
+//       1.5839206391675538,
+//       0,
+//       2
+//     ],
+//     "measures": [
+//       0.015011141606366117,
+//       0.03454565234304585
+//     ],
+//     "markers": [
+//       1.5637346292993346
+//     ]
+//   }
+// ]
 
 export default function Charts() {
 
+  const [chartData, setChartData] = useState([
+  {
+    "id": "c",
+    "label": "c",
+    "value": 511,
+    "color": "hsl(222, 70%, 50%)"
+  },
+  {
+    "id": "go",
+    "label": "go",
+    "value": 183,
+    "color": "hsl(279, 70%, 50%)"
+  },
+  {
+    "id": "sass",
+    "label": "sass",
+    "value": 556,
+    "color": "hsl(326, 70%, 50%)"
+  },
+  {
+    "id": "elixir",
+    "label": "elixir",
+    "value": 566,
+    "color": "hsl(310, 70%, 50%)"
+  },
+  {
+    "id": "python",
+    "label": "python",
+    "value": 228,
+    "color": "hsl(122, 70%, 50%)"
+  }
+]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+
+    //setChartData(Math.floor(Math.random() * 200))
+
+    setChartData([
+    {
+      "id": "c",
+      "label": "c",
+      "value": Math.floor(Math.random() * 200),
+      "color": "hsl(222, 70%, 50%)"
+    },
+    {
+      "id": "go",
+      "label": "go",
+      "value": Math.floor(Math.random() * 200),
+      "color": "hsl(279, 70%, 50%)"
+    },
+    {
+      "id": "sass",
+      "label": "sass",
+      "value": Math.floor(Math.random() * 200),
+      "color": "hsl(326, 70%, 50%)"
+    },
+    {
+      "id": "elixir",
+      "label": "elixir",
+      "value": Math.floor(Math.random() * 200),
+      "color": "hsl(310, 70%, 50%)"
+    },
+    {
+      "id": "python",
+      "label": "python",
+      "value": Math.floor(Math.random() * 200),
+      "color": "hsl(122, 70%, 50%)"
+    }
+  ])
+
+    }, 4000);
+
+    return () => clearInterval(intervalId); //This is important
+  })
+
   return (
     <div className="charts">
-      <ResponsiveBullet
-        data={data}
-        margin={{ top: 250, right: 25, bottom: 250, left: 25 }}
-        spacing={5}
-        // titleAlign="start"
-        titleOffsetX={-20}
-        titlePosition="after"
-        measureSize={0.2}
-        tooltip={() => (<></>)}
-        //onMarkerClick={}
-        />
+      <ResponsivePie /* or Pie for fixed dimensions */
+        data={chartData}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.6}
+        cornerRadius={2}
+        activeOuterRadiusOffset={8}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+        legends={[
+            {
+                anchor: 'bottom',
+                direction: 'row',
+                translateY: 56,
+                itemWidth: 100,
+                itemHeight: 18,
+                symbolShape: 'circle'
+            }
+        ]}
+    />
     </div>
   )
 }
