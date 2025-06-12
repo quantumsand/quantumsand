af697bf3b551bab7242453453a9fdc707931f8ba
Andrew Potter <hello@drewpotter.com>
2025-06-12 14:46:59 +0100

    Start working towards USE flags

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5,6 +5,12 @@ class QuantumsandLive
     sudo_password = STDIN.noecho(&:gets).chomp
     dirpath = "/mnt/gentoo"
 
+    gentoo_use_flags = %w(kde gtk plymouth wallpapers wacom networkmanager smart
+                          accessibility bluetooth crypt cups discover flatpak
+                          oxygen-theme grub webengine systemd sdk rdp pulseaudio
+                          elogind browser-integration crash-handler +display-manager).join(' ')
+    puts "Gentoo USE flags: #{gentoo_use_flags}"
+
     QuantumsandLive::FormatDrive.drive_partitioning(sudo_password)
     QuantumsandLive::FormatDrive.drive_formatting(sudo_password)
 
