b9817c1596afb1ebaaab957c1a1704caed899053
Andrew Potter <hello@drewpotter.com>
2025-09-10 14:23:41 +0100

    TODO huggingface/huggingface_hub

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -338,6 +338,7 @@ class QuantumsandLive
     # TODO: sys-fs/exfatprogs; exFAT file system
     # TODO: sys-fs/diskdev_cmds; a port of HFS/HFS+ utilities from OpenDarwin
     # TODO: uutils/coreutils; Cross-platform Rust rewrite of the GNU coreutils
+    # TODO: huggingface/huggingface_hub; official Python client for the Huggingface Hub
   end
 end
 
