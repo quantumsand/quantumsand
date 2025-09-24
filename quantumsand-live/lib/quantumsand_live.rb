ef77a69376cf6917467cc2c850fa6daf57396821
Andrew Potter <hello@drewpotter.com>
2025-09-24 14:40:38 +0100

    git_repos_xfce_gitlab panel-plugins/xfce4-indicator-plugin

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -225,7 +225,8 @@ class QuantumsandLive
       "panel-plugins/xfce4-eyes-plugin",
       "panel-plugins/xfce4-fsguard-plugin",
       "panel-plugins/xfce4-generic-slider",
-      "panel-plugins/xfce4-genmon-plugin"
+      "panel-plugins/xfce4-genmon-plugin",
+      "panel-plugins/xfce4-indicator-plugin"
     ]
 
     git_repos_kitware_gitlab = [
