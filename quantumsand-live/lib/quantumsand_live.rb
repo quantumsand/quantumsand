cbf17c5ee522c260dbf401beaa388e231c2b7e15
Andrew Potter <hello@drewpotter.com>
2025-09-24 15:02:35 +0100

    git_repos_xfce_gitlab panel-plugins/xfce4-verve-plugin

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -240,7 +240,8 @@ class QuantumsandLive
       "panel-plugins/xfce4-stopwatch-plugin",
       "panel-plugins/xfce4-systemload-plugin",
       "panel-plugins/xfce4-time-out-plugin",
-      "panel-plugins/xfce4-timer-plugin"
+      "panel-plugins/xfce4-timer-plugin",
+      "panel-plugins/xfce4-verve-plugin"
     ]
 
     git_repos_kitware_gitlab = [
