f72aec30a762d480c84f7570449c6b26087b6c1f
Andrew Potter <hello@drewpotter.com>
2025-09-24 14:29:38 +0100

    git_repos_xfce_gitlab panel-plugins/xfce4-docklike-plugin

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -220,7 +220,8 @@ class QuantumsandLive
       "panel-plugins/xfce4-clipman-plugin",
       "panel-plugins/xfce4-cpufreq-plugin",
       "panel-plugins/xfce4-cpugraph-plugin",
-      "panel-plugins/xfce4-diskperf-plugin"
+      "panel-plugins/xfce4-diskperf-plugin",
+      "panel-plugins/xfce4-docklike-plugin"
     ]
 
     git_repos_kitware_gitlab = [
