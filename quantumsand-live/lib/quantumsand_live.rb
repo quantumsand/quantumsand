a8fe6f7715bed15856ae149caae9f3442e89bb55
Andrew Potter <hello@drewpotter.com>
2025-09-24 15:04:03 +0100

    git_repos_xfce_gitlab panel-plugins/xfce4-weather-plugin

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -242,7 +242,8 @@ class QuantumsandLive
       "panel-plugins/xfce4-time-out-plugin",
       "panel-plugins/xfce4-timer-plugin",
       "panel-plugins/xfce4-verve-plugin",
-      "panel-plugins/xfce4-wavelan-plugin"
+      "panel-plugins/xfce4-wavelan-plugin",
+      "panel-plugins/xfce4-weather-plugin"
     ]
 
     git_repos_kitware_gitlab = [
