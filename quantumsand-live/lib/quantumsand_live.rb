38a10f6ff8c1ce9eeabf410a4d1e83e7e313703f
Andrew Potter <hello@drewpotter.com>
2025-09-26 19:23:55 +0100

    git_repos_github processone/ejabberd

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -239,7 +239,8 @@ class QuantumsandLive
       "AndreasFaust/react-ticker",
       "paulcollett/react-masonry-css",
       "pmndrs/react-spring",
-      "3DJakob/react-tinder-card"
+      "3DJakob/react-tinder-card",
+      "processone/ejabberd"
     ]
 
     git_repos_gitlab = [
