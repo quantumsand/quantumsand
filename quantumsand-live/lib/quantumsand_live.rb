ebe3524063ae65b68053e1f5db8ee70092f9c849
Andrew Potter <hello@drewpotter.com>
2025-10-27 13:12:29 +0000

    git_repos_chromium_googlesource chromium/tools

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -842,7 +842,8 @@ class QuantumsandLive
 
     git_repos_chromium_googlesource = [
       "chromium/src",
-      "v8/v8"
+      "v8/v8",
+      "chromium/tools"
     ]
 
     git_repos_gn_googlesource = [
