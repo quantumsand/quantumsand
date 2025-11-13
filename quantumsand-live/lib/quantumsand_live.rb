9f1bf4da3af5d25de4403fe3a20faf33ed792203
Andrew Potter <hello@drewpotter.com>
2025-11-13 13:51:51 +0000

    git_repos_freedesktop_gitlab libopenraw/libopenraw

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -1370,7 +1370,8 @@ class QuantumsandLive
       "dbus/dbus-test",
       "xdg/desktop-file-utils",
       "libopenraw/exempi",
-      "libopenraw/exempi-rs"
+      "libopenraw/exempi-rs",
+      "libopenraw/libopenraw"
     ]
 
     git_repos_xfce_gitlab = [
