0f49296cc559243b1ecda8aad531a1bdab59365f
Andrew Potter <hello@drewpotter.com>
2025-12-25 13:39:27 +0000

    git_repos_gitlab qemu-project/sgabios

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -2409,7 +2409,8 @@ class QuantumsandLive
       "qemu-project/qemu-palcode",
       "qemu-project/QemuMacDrivers",
       "qemu-project/seabios",
-      "qemu-project/seabios-hppa"
+      "qemu-project/seabios-hppa",
+      "qemu-project/sgabios"
     ]
 
     git_repos_freedesktop_gitlab = [
