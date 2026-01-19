6b9fc659dba71e0a2536fac5ee84b4338ecaa153
Andrew Potter <hello@drewpotter.com>
2026-01-19 11:34:18 +0000

    git_repos_redox_os_gitlab redox-os/hyper-tls

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3278,7 +3278,8 @@ class QuantumsandLive
       "redox-os/homebrew-gcc_cross_compilers",
       "redox-os/hostname",
       "redox-os/hwio",
-      "redox-os/hyper"
+      "redox-os/hyper",
+      "redox-os/hyper-tls"
     ]
 
     git_repos_freebsd_org = [
