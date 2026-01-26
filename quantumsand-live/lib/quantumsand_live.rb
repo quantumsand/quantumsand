f7a635eb81b1664b8dd43790311c5c060337ed22
Andrew Potter <hello@drewpotter.com>
2026-01-26 15:45:06 +0000

    git_repos_github ros2/eigen3_cmake_module

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -2838,7 +2838,8 @@ class QuantumsandLive
       "ros2/design",
       "ros2/detection_visualizer",
       "ros2/domain_bridge",
-      "colcon/colcon-core"
+      "colcon/colcon-core",
+      "ros2/eigen3_cmake_module"
     ]
 
     git_repos_chromium_googlesource = [
