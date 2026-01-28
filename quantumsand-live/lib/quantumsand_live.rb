a78003fa8c7d0b60a70116815e6c86bfa6cda0a9
Andrew Potter <hello@drewpotter.com>
2026-01-28 12:31:11 +0000

    git_repos_redox_os_gitlab redox-os/redox-scheme

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3511,7 +3511,8 @@ class QuantumsandLive
       "redox-os/redox-loader",
       "redox-os/redox-log",
       "redox-os/parallel",
-      "redox-os/redox-posix-tests"
+      "redox-os/redox-posix-tests",
+      "redox-os/redox-scheme"
     ]
 
     git_repos_freebsd_org = [
