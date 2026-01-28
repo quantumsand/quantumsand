7e20b869a43ba373631d47243059f88c3fb0c427
Andrew Potter <hello@drewpotter.com>
2026-01-28 15:51:20 +0000

    git_repos_github ros2/rosidl_typesupport

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -2862,7 +2862,8 @@ class QuantumsandLive
       "ros2/message_filters",
       "ros2/rmw",
       "ros2/rmw_implementation",
-      "ros2/rosidl"
+      "ros2/rosidl",
+      "ros2/rosidl_typesupport"
     ]
 
     git_repos_chromium_googlesource = [
