8dd9c3cc667cb7b3ea786bf3a4d2925ff6143626
Andrew Potter <hello@drewpotter.com>
2026-02-02 15:20:54 +0000

    git_repos_github ros2/ros2_dds_profiles_examples

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -2944,7 +2944,8 @@ class QuantumsandLive
       "ros2/ros1_bridge",
       "ros2/ros2cli",
       "ros2/ros2cli_common_extensions",
-      "ros2/ros2doc"
+      "ros2/ros2doc",
+      "ros2/ros2_dds_profiles_examples"
     ]
 
     git_repos_chromium_googlesource = [
