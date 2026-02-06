4cb8e8cd3bb6e64dee2b2459e5ed3f4a848d4410
Andrew Potter <hello@drewpotter.com>
2026-02-06 17:02:37 +0000

    Done ROOT: data analysis framework

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -4376,7 +4376,6 @@ class QuantumsandLive
     # TODO: soapySDR
     # TODO: srsGUI + srsRAN_4G + srsRAN_Project (complete 5G RAN solution)
     # TODO: qradiolink
-    # TODO: ROOT: data analysis framework; sci-physics/root
     # TODO: Tesseract Open Source OCR Engine
     # TODO: WanzenBug/rqrr; QR reader written in Rust
     # TODO: Open access papers PDF science and AI; title:, authors:, license:, pdf_url:
