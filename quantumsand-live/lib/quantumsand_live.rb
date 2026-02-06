9bd72137821da6422c0acf8ab74e0bf0aa9fbda9
Andrew Potter <hello@drewpotter.com>
2026-02-06 11:44:10 +0000

    git_repos_github ros-perception/point_cloud_transport_plugins

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3089,7 +3089,8 @@ class QuantumsandLive
       "ros-perception/pcl-fuerte",
       "ros-perception/pcl_msgs",
       "ros-perception/perception_open3d",
-      "ros-perception/point_cloud_transport"
+      "ros-perception/point_cloud_transport",
+      "ros-perception/point_cloud_transport_plugins"
     ]
 
     git_repos_chromium_googlesource = [
