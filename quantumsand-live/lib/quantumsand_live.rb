b38b07eda084c546ea7d1972759d7d650b95dae6
Andrew Potter <hello@drewpotter.com>
2026-02-23 13:23:26 +0000

    git_repos_osmocom_org_gitea cellular-infrastructure/osmo-sip-connector

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3569,7 +3569,8 @@ class QuantumsandLive
       "cellular-infrastructure/osmo-mgw",
       "cellular-infrastructure/osmocom-analog",
       "phone-side/osmocom-docs",
-      "cellular-infrastructure/osmo-uecups"
+      "cellular-infrastructure/osmo-uecups",
+      "cellular-infrastructure/osmo-sip-connector"
     ]
 
     git_repos_eden_emu_dev = [
