2800cb81557b428bd0c08461866da4282ac0510d
Andrew Potter <hello@drewpotter.com>
2026-02-25 14:11:15 +0000

    git_repos_osmocom_org_gitea erlang/logger_gsmtap

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3622,7 +3622,8 @@ class QuantumsandLive
       "ims-volte-vowifi/SWu-IKEv2",
       "ims-volte-vowifi/doubango",
       "erlang/osmo-s1gw",
-      "erlang/osmo_dia2gsup"
+      "erlang/osmo_dia2gsup",
+      "erlang/logger_gsmtap"
     ]
 
     git_repos_eden_emu_dev = [
