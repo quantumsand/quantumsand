24e485b9c586a73c7c7e0a80791f5d3444f503be
Andrew Potter <hello@drewpotter.com>
2026-02-25 11:28:29 +0000

    git_repos_osmocom_org_gitea cellular-infrastructure/osmo-rbs

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -3601,7 +3601,8 @@ class QuantumsandLive
       "cellular-infrastructure/asn1-ss7",
       "cellular-infrastructure/osmo-abi-check",
       "cellular-infrastructure/osmo-oohmi",
-      "cellular-infrastructure/osmocom-lcs"
+      "cellular-infrastructure/osmocom-lcs",
+      "cellular-infrastructure/osmo-rbs"
     ]
 
     git_repos_eden_emu_dev = [
