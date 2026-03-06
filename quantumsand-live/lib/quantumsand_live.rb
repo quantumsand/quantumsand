043003b8d53bdebfc9bd1882bed6aa6002e6a103
Andrew Potter <hello@drewpotter.com>
2026-03-06 09:07:16 +0000

    git_repos_kernel_org fs/xfs/xfsprogs-dev

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -4004,7 +4004,8 @@ class QuantumsandLive
       "linux/kernel/git/firmware/linux-firmware",
       "linux/kernel/git/jberg/rfkill",
       "linux/kernel/git/tyhicks/ecryptfs",
-      "linux/kernel/git/vfs/vfs"
+      "linux/kernel/git/vfs/vfs",
+      "fs/xfs/xfsprogs-dev"
     ]
 
     git_repos_isc_org_gitlab = [
