2996d4403d6f2252c91acccaae57feb3b18a7770
Andrew Potter <hello@drewpotter.com>
2026-03-08 14:58:04 +0000

    Add NarrativeSyncWaveform component

--- quantumsand-ui-react/src/pages/NarrativeSync/NarrativeSync.js
+++ quantumsand-ui-react/src/pages/NarrativeSync/NarrativeSync.js
@@ -2,13 +2,16 @@ import * as React from 'react';
 
 import "./NarrativeSync.css";
 import NarrativeSyncButtons from './NarrativeSyncButtons';
+import NarrativeSyncWaveform from './NarrativeSyncWaveform';
 
-export default function NarrativeSync({ src, srcArrayBuffer, srcJson, markdown }) {
+export default function NarrativeSync({ videoUrl = "/video/sample-narrative-dynamic.webm",
+                                        srcJson = "/video/sample-narrative-dynamic.json", markdown }) {
 
   return (
     <div className="narrative__sync">
       <p>TODO: NarrativeSync components:</p>
       <p>Waveform; narrative__syncWaveform</p>
+      <NarrativeSyncWaveform videoUrl={videoUrl} srcJson={srcJson} />
       <p>Narrative video; narrative__syncVideo / Narrative text; narrative__syncText</p>
       <p>Narrative spreadsheet; narrative__syncSpreadsheet / Narrative buttons; narrative__syncButtons</p>
       <NarrativeSyncButtons />
---
+++ quantumsand-ui-react/src/pages/NarrativeSync/NarrativeSyncWaveform.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import Peaks from 'peaks.js';
+
+import "./NarrativeSyncWaveform.css";
+
+export default function NarrativeSyncWaveform({ videoUrl, srcJson }) {
+
+  const [peaksInstance, setPeaksInstance] = useState([]);
+
+  useEffect(() => Peaks.init({
+    containers: {
+      zoomview: document.getElementById('zoomview-container'),
+      overview: document.getElementById('overview-container')
+    },
+    zoomview: {
+      container: document.getElementById('zoomview-container')
+    },
+    overview: {
+      container: document.getElementById('overview-container')
+    },
+    mediaElement: document.querySelector('video'),
+    dataUri: {
+      //arraybuffer: srcArrayBuffer,
+      json: srcJson
+    },
+    keyboard: true,
+    pointMarkerColor: '#006eb0',
+    showPlayheadTime: true,
+  }, function(err, peaks) {
+
+    if (err) {
+      console.error('Failed to initialize Peaks instance: ' + err.message);
+      return;
+    }
+
+    console.log(peaks.player.getCurrentTime());
+    setPeaksInstance(peaks);
+  }), [srcJson]);
+
+  const zoomIn = e => {
+    e.preventDefault();
+    console.log("zoom in");
+    peaksInstance.zoom.zoomIn();
+  }
+
+  const zoomOut = e => {
+    e.preventDefault();
+    console.log("zoom out");
+    peaksInstance.zoom.zoomOut();
+  }
+
+  return (
+    <div className="narrative__syncWaveform">
+      <p>Narrative waveform; narrative__syncWaveform</p>
+      <div id="waveform-container">
+        <div id="zoomview-container"></div>
+        <div id="overview-container"></div>
+        <video id="audio" controls="controls" className="narrative__syncWaveformVideo">
+          <source src={videoUrl} type="video/webm" />
+          Your browser does not support the audio element.
+        </video>
+      </div>
+    </div>
+  )
+}
