bdfc08c5c9988476cf7bae526a7b6bfdf833e727
Andrew Potter <hello@drewpotter.com>
2026-03-20 14:52:48 +0000

    git_repos_github test-prof/test-prof

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -4003,7 +4003,8 @@ class QuantumsandLive
       "ros/ros_comm",
       "ros/ros_comm_msgs",
       "ros/ros_environment",
-      "ros/ros_message_user_examples"
+      "ros/ros_message_user_examples",
+      "test-prof/test-prof"
     ]
 
     git_repos_chromium_googlesource = [
