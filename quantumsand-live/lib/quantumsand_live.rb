46fd50215c9042d19a83e1120fefce98f6b88891
Andrew Potter <hello@drewpotter.com>
2026-04-04 09:23:52 +0100

    git_repos_github datalayer/jupyter-mcp-server

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -4243,7 +4243,8 @@ class QuantumsandLive
       "awslabs/aws-c-s3",
       "awslabs/aws-c-sdkutils",
       "intel/linux-npu-driver",
-      "openrazer/openrazer"
+      "openrazer/openrazer",
+      "datalayer/jupyter-mcp-server"
     ]
 
     git_repos_chromium_googlesource = [
