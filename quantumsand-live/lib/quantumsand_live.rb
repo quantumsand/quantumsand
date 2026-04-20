624bd6d8415e7351b428a2de492168fb7deb8950
Andrew Potter <hello@drewpotter.com>
2026-04-20 12:43:41 +0100

    git_repos_github mapbox/shelf-pack

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -4386,7 +4386,8 @@ class QuantumsandLive
       "mapbox/cheap-ruler",
       "vipera/cheap-ruler-rs",
       "Turbo87/flat-projection-rs",
-      "mapbox/shelf-pack-cpp"
+      "mapbox/shelf-pack-cpp",
+      "mapbox/shelf-pack"
     ]
 
     git_repos_chromium_googlesource = [
