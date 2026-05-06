ddc0346bafe4d18326ce83461eb3bfafe5188348
Andrew Potter <hello@drewpotter.com>
2026-05-06 10:11:51 +0100

    git_repos_code_videolan_org videolan/dav1d

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5523,7 +5523,8 @@ class QuantumsandLive
       "videolan/bitstream",
       "videolan/breakpad",
       "videolan/checkasm",
-      "videolan/CrashDragon"
+      "videolan/CrashDragon",
+      "videolan/dav1d"
     ]
 
     @git_repos_xiph_org_gitlab = [
