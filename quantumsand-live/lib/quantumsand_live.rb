cd04e2e01ecbfc543567351f6d686ac89432a39a
Andrew Potter <hello@drewpotter.com>
2026-05-29 14:42:29 +0100

    git_repos_gitlab ohwr/project/fmc-adc-250m-16b-4cha

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5717,7 +5717,8 @@ class QuantumsandLive
       "ohwr/project/fmc-adc-100m14b4cha-tst",
       "ohwr/project/pts-base",
       "ohwr/project/fmc-adc-130m-16b-4cha",
-      "ohwr/project/fmc-adc-1g8b2cha"
+      "ohwr/project/fmc-adc-1g8b2cha",
+      "ohwr/project/fmc-adc-250m-16b-4cha"
     ]
 
     @git_repos_freedesktop_gitlab = [
