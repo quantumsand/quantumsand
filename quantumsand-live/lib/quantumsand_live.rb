66aa3de3d9a12bb35ecb125a3c34b573704df643
Andrew Potter <hello@drewpotter.com>
2026-05-29 10:07:51 +0100

    git_repos_gitlab ohwr/level-conversion/conv-ttl-rs485/conv-ttl-rs485-hw

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5672,7 +5672,8 @@ class QuantumsandLive
       "ohwr/level-conversion/conv-ttl-blo/conv-ttl-blo-tst",
       "ohwr/level-conversion/conv-ttl-blo/conv-ttl-blo-hw",
       "ohwr/project/conv-ttl-blo",
-      "ohwr/project/conv-ttl-nim-3i-30o"
+      "ohwr/project/conv-ttl-nim-3i-30o",
+      "ohwr/level-conversion/conv-ttl-rs485/conv-ttl-rs485-hw"
     ]
 
     @git_repos_freedesktop_gitlab = [
