df5c710a57c4c6b161ba7c5417d6fb0ce6c9d755
Andrew Potter <hello@drewpotter.com>
2026-06-04 14:18:11 +0100

    git_repos_gitlab ohwr/project/afc-ipmi-sw

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5850,7 +5850,8 @@ class QuantumsandLive
       "ohwr/project/mrpc-25x18-24strip",
       "ohwr/project/r19-tdc-del-a",
       "ohwr/project/nanofip-test-board",
-      "ohwr/project/ohw-research"
+      "ohwr/project/ohw-research",
+      "ohwr/project/afc-ipmi-sw"
     ]
 
     @git_repos_freedesktop_gitlab = [
