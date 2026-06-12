905619f81c1f9e344aa85c0cb626e2533978e91b
Andrew Potter <hello@drewpotter.com>
2026-06-12 10:14:28 +0100

    git_repos_github pop-os/gnome-shell-extension-do-not-disturb

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5053,7 +5053,8 @@ class QuantumsandLive
       "pop-os/gnome-settings-daemon",
       "pop-os/gnome-shell",
       "pop-os/gnome-shell-extension-alt-tab-raise-first-window",
-      "pop-os/gnome-shell-extension-always-show-workspaces"
+      "pop-os/gnome-shell-extension-always-show-workspaces",
+      "pop-os/gnome-shell-extension-do-not-disturb"
     ]
 
     @git_repos_chromium_googlesource = [
