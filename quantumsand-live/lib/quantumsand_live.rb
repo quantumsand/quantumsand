c3d45ffebc464d04fd2ed5af307e40625718a6d0
Andrew Potter <hello@drewpotter.com>
2026-06-24 11:09:35 +0100

    git_repos_github pop-os/system76-wallpapers

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5370,7 +5370,8 @@ class QuantumsandLive
       "pop-os/system76-io-dkms",
       "pop-os/system76-oled",
       "pop-os/system76-scheduler",
-      "pop-os/system76-ubuntu-repo"
+      "pop-os/system76-ubuntu-repo",
+      "pop-os/system76-wallpapers"
     ]
 
     @git_repos_chromium_googlesource = [
