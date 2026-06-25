8bb10ac097f0ab9f7e5e6d8d0fc41810bcd74265
Andrew Potter <hello@drewpotter.com>
2026-06-25 12:02:15 +0100

    Add git_repos_swiftshader_googlesource SwiftShader

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -14,6 +14,7 @@ class QuantumsandLive
     @git_repos_skia_googlesource_url = ENV.fetch("GIT_REPOS_SKIA_GOOGLESOURCE_URL")
     @git_repos_android_googlesource_url = ENV.fetch("GIT_REPOS_ANDROID_GOOGLESOURCE_URL")
     @git_repos_dawn_googlesource_url = ENV.fetch("GIT_REPOS_DAWN_GOOGLESOURCE_URL")
+    @git_repos_swiftshader_googlesource_url = ENV.fetch("GIT_REPOS_SHIFTSHADER_GOOGLESOURCE_URL")
     @git_repos_huggingface_co_url = ENV.fetch("GIT_REPOS_HUGGINGFACE_CO_URL")
     @git_repos_huggingface_co_dataset_url = ENV.fetch("GIT_REPOS_HUGGINGFACE_CO_DATASET_URL")
     @git_repos_libreoffice_org_url = ENV.fetch("GIT_REPOS_LIBREOFFICE_ORG_URL")
@@ -5476,6 +5477,10 @@ class QuantumsandLive
 
     @git_repos_dawn_googlesource = [
       "dawn"
+    ]
+
+    @git_repos_swiftshader_googlesource = [
+      "SwiftShader"
     ]
 
     @git_xet_hf_download_huggingface_co = [
