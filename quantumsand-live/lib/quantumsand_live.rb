44ad4960b37c4334b35809f07fa2421a7a5d70e4
Andrew Potter <hello@drewpotter.com>
2026-07-31 07:14:20 +0100

    git_repos_github brewfs/brewfs

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -5881,7 +5881,8 @@ class QuantumsandLive
       "apache/datafusion-python",
       "apache/datafusion-comet",
       "apache/datafusion-ballista",
-      "apache/opendal"
+      "apache/opendal",
+      "brewfs/brewfs"
     ]
 
     @git_repos_github_lfs = [
