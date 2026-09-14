a0e4e15faad0c096fa34cacd0dbf1c03e7b9abd7
Andrew Potter <hello@drewpotter.com>
2026-09-14 17:24:43 +0100

    git_repos_github rusqlite/rusqlite

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -6119,7 +6119,8 @@ class QuantumsandLive
       "Clay-foundation/model",
       "NASA-IMPACT/NASA-IBM-Lunar-Foundation-Model",
       "NASA-IMPACT/hls-foundation-os",
-      "cloudtostreet/Sen1Floods11"
+      "cloudtostreet/Sen1Floods11",
+      "rusqlite/rusqlite"
     ]
 
     @git_repos_github_lfs = [
