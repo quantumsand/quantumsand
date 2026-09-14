18d23aefaff701df020b6c9d9d829cc6b352ff91
Andrew Potter <hello@drewpotter.com>
2026-09-14 08:10:53 +0100

    git_xet_hf_download_huggingface_co ibm-nasa-geospatial/Prithvi-EO-1.0-100M

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -6271,7 +6271,8 @@ class QuantumsandLive
       "openai/whisper-medium",
       "openai/whisper-large-v2",
       "meta-models/Muse-Glimmer-30B",
-      "nasa-ibm-ai4science/NASA-IBM-Lunar-Foundation-Model"
+      "nasa-ibm-ai4science/NASA-IBM-Lunar-Foundation-Model",
+      "ibm-nasa-geospatial/Prithvi-EO-1.0-100M"
     ]
 
     @git_xet_hf_download_huggingface_co_dataset = [
