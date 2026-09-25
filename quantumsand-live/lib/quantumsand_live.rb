e6abdda49547b9be326f97c047c579b90df38b8e
Andrew Potter <hello@drewpotter.com>
2026-09-25 12:25:08 +0100

    gentoo_use_flags control-socket

--- quantumsand-live/lib/quantumsand_live.rb
+++ quantumsand-live/lib/quantumsand_live.rb
@@ -9339,7 +9339,7 @@ class QuantumsandLive
                           music pdfannotextractor pstricks publishers science tex4ht texi2html xetex
                           ntfsprogs ntfsdecrypt mount-ntfs arm64ec georeplication grafana rsyslog
                           libtirpc active-response flexresp3 gre non-ether-decoders ppm react threads
-                          -branding -java).join(' ')
+                          control-socket -branding -java).join(' ')
 
     puts "Gentoo USE flags: #{gentoo_use_flags}"
 
